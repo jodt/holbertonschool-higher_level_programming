3ef521736cac6cf768aaad4be76a7b03a4d7d1a2
jodt <joel.dumortier@gmail.com>
2021-12-21 13:31:52 +0000

    bug fix too much character

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import random
-number = random.randint(-10000, 10000)
+#number = random.randint(-10000, 10000)
+number = -98
 str = "Last digit of"
 str_1 = "and is less than 6 and not 0"
 if number < 0:
@@ -10,6 +11,6 @@ else:
 if last_digit == 0:
     print("{} {} is {} and is 0".format(str, number, last_digit))
 elif last_digit < 6:
-    print("{} {} is {} {} ".format(str, number, last_digit, str_1))
+    print("{} {} is {}".format(str, number, last_digit), str_1)
 else:
     print("{} {} is {} and is greater than 5".format(str, number, last_digit))
