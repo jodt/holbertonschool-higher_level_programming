de4f7e1c2495c5f2ae1497c604359adf73192e74
jodt <joel.dumortier@gmail.com>
2021-12-23 11:36:18 +0000

    bug fix if file is imported

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
-import sys
-arg_list = sys.argv
-number_of_arg = len(arg_list)-1
-if number_of_arg == 0:
-    arg_text = "arguments."
-elif number_of_arg == 1:
-    arg_text = "argument:"
-else:
-    arg_text = "arguments:"
-print("{} {}".format(number_of_arg, arg_text))
-for arg in range(1, len(arg_list)):
-    print("{}: {}".format(arg, arg_list[arg]))
+if __name__ == "__main__":
+    import sys
+    arg_list = sys.argv
+    number_of_arg = len(arg_list)-1
+    if number_of_arg == 0:
+        arg_text = "arguments."
+    elif number_of_arg == 1:
+        arg_text = "argument:"
+    else:
+        arg_text = "arguments:"
+    print("{} {}".format(number_of_arg, arg_text))
+    for arg in range(1, len(arg_list)):
+        print("{}: {}".format(arg, arg_list[arg]))
