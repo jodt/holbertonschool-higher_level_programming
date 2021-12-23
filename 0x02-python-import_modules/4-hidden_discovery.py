ff8388540ededebdd2118165e160841849c15021
jodt <joel.dumortier@gmail.com>
2021-12-23 11:38:34 +0000

    bug fix if file is imported

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-import hidden_4
-names_list = dir(hidden_4)
-for name in names_list:
-    if (name[0] == "_"):
-        continue
-    else:
-        print("{}".format(name))
+if __name__ == "__main__":
+    import hidden_4
+    names_list = dir(hidden_4)
+    for name in names_list:
+        if (name[0] == "_"):
+            continue
+        else:
+            print("{}".format(name))
