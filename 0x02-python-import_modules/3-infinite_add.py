e01fa5bc091f579be05351656135e624ac1fb50e
jodt <joel.dumortier@gmail.com>
2021-12-23 11:37:18 +0000

    bug fix if file is imported

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-import sys
-arg_list = sys.argv
-result = 0
-for arg in range(1, len(arg_list)):
-    result += int(arg_list[arg])
-print(result)
+if __name__ == "__main__":
+    import sys
+    arg_list = sys.argv
+    result = 0
+    for arg in range(1, len(arg_list)):
+        result += int(arg_list[arg])
+    print(result)
