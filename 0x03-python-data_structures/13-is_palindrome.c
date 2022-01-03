cb7c33b8eeae4a3de19f13dab7085d22d0c39e08
jodt <joel.dumortier@gmail.com>
2022-01-03 14:04:13 +0000

    betty bug fix

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -2,13 +2,21 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+/**
+ * is_palindrome - check if linked list is a palindrome
+ * @head: address of a pointer on the first element fo the list
+ *
+ * Return: 1 if the list is a plaindrome else 0
+ */
 int is_palindrome(listint_t **head)
 {
 	listint_t *last, *first;
 	int length = 1, count = 0, idx;
 
+	if (head == NULL)
+		return (0);
 	if (*head == NULL)
-		return 1;
+		return (1);
 	last = first = *head;
 	while (last->next)
 	{
@@ -27,7 +35,7 @@ int is_palindrome(listint_t **head)
 			idx++;
 		}
 		if (idx - count == 0)
-			return 1;
+			return (1);
 	}
-	return 0;
+	return (0);
 }
