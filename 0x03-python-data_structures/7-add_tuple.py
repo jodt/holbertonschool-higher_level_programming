e8e0973e1e95d2fe35af090fc1a8ce532a313c7a
jodt <joel.dumortier@gmail.com>
2022-01-03 11:59:41 +0000

    bug fix if tuple is bigger than 2

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -5,14 +5,16 @@ def add_tuple(tuple_a=(), tuple_b=()):
     elif len(tuple_a) == 1:
         first_a = tuple_a[0]
         second_a = 0
-    else:
-        first_a, second_a = tuple_a
+    elif (len(tuple_a) >= 2):
+        first_a = tuple_a[0]
+        second_a = tuple_a[1]
     if len(tuple_b) == 0:
         first_b, second_b = 0, 0
     elif len(tuple_b) == 1:
-        first_b = tuple_b[0]
+        first_b = tuple_b
         second_b = 0
-    else:
-        first_b, second_b = tuple_b
+    elif (len(tuple_b) >= 2):
+        first_b = tuple_b[0]
+        second_b = tuple_b[1]
     c = first_a + first_b, second_a + second_b
     return c
