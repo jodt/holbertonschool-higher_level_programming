748f8c453cb59da37b5e768d7eaff581e001e91c
jodt <joel.dumortier@gmail.com>
2022-01-05 12:58:13 +0000

    bux fix return key instead of value

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,11 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    new_list = dict()
     if a_dictionary:
-        for k, v in a_dictionary.items():
-            if a_dictionary[k] is not None:
-                new_list[k] = v
-        if len(new_list) == 0:
-            return None
-        else:
-            return max(list(new_list.values()))
+        return max(a_dictionary.keys())
+    else:
+        return None
