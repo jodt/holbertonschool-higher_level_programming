a4cb376052cb23010c72dddfebe75ad5ddbe9599
jodt <joel.dumortier@gmail.com>
2022-01-05 13:20:49 +0000

    pycodestyle bug fix

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    roman_int = {'I': 1, 'V': 5,
-                 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    ri = {'I': 1, 'V': 5,
+          'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     result = 0
     if (type(roman_string) is not str or roman_string is None):
         return 0
     for i, c in enumerate(roman_string):
-        if ((i+1) == len(roman_string) or roman_int[c] >= roman_int[roman_string[i+1]]):
-            result += roman_int[c]
+        if ((i+1) == len(roman_string) or ri[c] >= ri[roman_string[i+1]]):
+            result += ri[c]
         else:
-            result -= roman_int[c]
+            result -= ri[c]
     return result
