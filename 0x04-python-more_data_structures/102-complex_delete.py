0664f23998c4cc1ed4212b7e078067d7b0af068a
jodt <joel.dumortier@gmail.com>
2022-01-05 15:43:13 +0000

    indentation bug fix

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
     key = []
-    for k, v in a_dictionary.items():
-        if value in a_dictionary.values():
+    if value in a_dictionary.values():
+        for k, v in a_dictionary.items():
             if v == value:
                 key.append(k)
-                for ky in key:
-                    del(a_dictionary[ky])
-            return a_dictionary
+        for ky in key:
+            del(a_dictionary[ky])
+    return a_dictionary
