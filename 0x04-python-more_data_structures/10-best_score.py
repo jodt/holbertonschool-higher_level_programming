f4ce95ff28a0294ac139c193cf7bed69e94370dd
jodt <joel.dumortier@gmail.com>
2022-01-05 12:34:09 +0000

    bug fix if values is none

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
+    new_list = dict()
     if a_dictionary:
-        return (max(list(a_dictionary.values())))
-    else:
-        return None
+        for k, v in a_dictionary.items():
+            if a_dictionary[k] is not None:
+                new_list[k] = v
+        if len(new_list) == 0:
+            return None
+        else:
+            return max(list(new_list.values()))
