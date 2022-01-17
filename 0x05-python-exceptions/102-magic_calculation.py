90f5a084ba6b4da196c057d952097779d5fed6ef
jodt <joel.dumortier@gmail.com>
2022-01-17 15:30:54 +0000

    refactoring code

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,15 +1,12 @@
 #!/usr/bin/python3
 def magic_calculation(a, b):
     result = 0
-    for i in range(1, 3):
-        if (i > a):
-            raise Exception("Too far")
-        try:
+    try:
+        for i in range(1, 3):
+            if (i > a):
+                raise Exception("Too far")
             result += a ** b / i
             break
-        except:
-            result = a + b
-            break
+    except Exception:
+        result = a + b
     return result
-
-    
