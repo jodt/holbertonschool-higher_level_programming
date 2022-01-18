f20c49e0851284ca52cfb36e75fa52826198490a
jodt <joel.dumortier@gmail.com>
2022-01-18 15:37:02 +0000

    bux fix picodestyle

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -8,10 +8,11 @@ class Square:
     """
 
     def __init__(self, size):
-        """ Initialise Data 
+        """ Initialise Data
 
         Parameters
         ----------
-        size : size of the square
+        size : int
+            size of the square.
         """
         self.__size = size
