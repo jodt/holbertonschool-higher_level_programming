243e3e13c93e8d39f1c0d6e26b68c178ff6f0f66
jodt <joel.dumortier@gmail.com>
2022-01-18 15:59:12 +0000

    documentation bug fix

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,11 +1,26 @@
 #!/usr/bin/python3
+"""This module create a square"""
+
+
 class Square:
     """
-    This class represents a square
-    """
+    This class represents a square """
 
     def __init__(self, size=0):
-        """ Initialise Data and checks if size is and integer and not < 0"""
+        """ Initialise Data and checks if size is and integer and not < 0
+
+        Parameters:
+        ----------
+        size: int
+            size of the square
+
+        Raises:
+        -------
+        Typerror
+            If size is not an integer
+        ValueError
+            If size is > 0
+        """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif (size < 0):
@@ -14,5 +29,11 @@ class Square:
             self.__size = size
 
     def area(self):
-        """return aera of the square"""
+        """calcul the aera of the square
+
+        Returns
+        -------
+        int
+            the aera of the square
+        """
         return self.__size ** 2
