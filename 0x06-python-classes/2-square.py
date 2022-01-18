ac43960818c6f24bc739b00c1596c2f8dfb1df75
jodt <joel.dumortier@gmail.com>
2022-01-18 15:56:17 +0000

    documentation bug fix

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,11 +1,27 @@
 #!/usr/bin/python3
+"""This module create a square"""
+
+
 class Square:
     """
     This class represents a square
     """
 
     def __init__(self, size=0):
-        """ Initialise Data and checks if size is and integer and not < 0"""
+        """ Initialise Data and checks if size is and integer and not < 0
+
+        Parameters:
+        ----------
+        size: int
+            size of the square
+
+        Raises:
+        -------
+        Typerror
+            If size is not an integer
+        ValueError
+            If size is > 0
+        """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif (size < 0):
