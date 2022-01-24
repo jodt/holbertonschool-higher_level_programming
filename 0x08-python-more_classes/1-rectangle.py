a5da507879ee1eaaa1c87a2d4649ddd1159a5ba9
jodt <joel.dumortier@gmail.com>
2022-01-24 19:25:58 +0000

    indentation bug fix

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
 This module create a rectangle
+
 """
 
 
@@ -22,37 +23,37 @@ class Rectangle:
         self.width = width
         self.height = height
 
-        @property
-        def width(self):
-            """
-            Getter to access of the width value
+    @property
+    def width(self):
+        """
+        Getter to access of the width value
 
-            Return
+        Return
+        ------
+        int : value of width
+         """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """
+        Setter to update width value
+
+        Parameter:
+        ----------
+        Value (int): value of width
+
+        Raises
             ------
-            int : value of width
-            """
-            return self.__width
-
-        @width.setter
-        def width(self, value):
-            """
-            Setter to update width value
-
-            Parameter:
-            ----------
-            Value (int): value of width
-
-            Raises
-            ------
-            TypeError: if value is not integer
+        TypeError: if value is not integer
             ValueError: if value is < 0
             """
 
-            if not isinstance(value, int):
-                raise TypeError("width must be an integer")
-            if value < 0:
-                raise ValueError("width must be >= 0")
-            self.__width = value
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
 
         @property
         def height(self):
