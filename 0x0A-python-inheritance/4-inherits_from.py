814665411577137259b8365eaa8873eed694e2be
jodt <joel.dumortier@gmail.com>
2022-01-31 17:18:10 +0000

    change != operator by is not

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -7,5 +7,5 @@ This is the inherits_from module
 def inherits_from(obj, a_class):
     """returns True if the object is an instance of a class
     that inherited (directly or indirectly) from the specified class"""
-    if (type(obj)) != a_class:
+    if (type(obj)) is not a_class:
         return (issubclass(type(obj), a_class))
