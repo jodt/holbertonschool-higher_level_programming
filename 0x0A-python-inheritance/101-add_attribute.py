a75447d51e557235b7fccc6ea98707b20aa8b490
jodt <joel.dumortier@gmail.com>
2022-01-31 17:12:34 +0000

    bug fix documentation

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,6 +1,23 @@
 #!/usr/bin/python3
+"""
+This is add_attribute module
+"""
+
 
 def add_attribute(obj, str1, str2):
+    """
+    Add a new attribute in an object if possible
+
+    Parameters:
+    -----------
+    obj : object
+    str1 : name of the attribute
+    str2 : value of the attribute
+
+    Raise:
+    ------
+    TypeError: if it's not possible to add new attribute
+    """
     if obj.__class__.__module__ == "builtins":
         raise TypeError("can't add new attribute ")
     else:
