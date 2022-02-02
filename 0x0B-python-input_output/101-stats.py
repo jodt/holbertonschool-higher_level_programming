1851880a48257adcda0a81041960c0b316318ce4
jodt <joel.dumortier@gmail.com>
2022-02-02 13:55:39 +0000

    bug fix

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -14,9 +14,8 @@ try:
     for line in sys.stdin:
         separate_string = line.split()
         filesize += int(separate_string[-1])
-        for k in result.keys():
-            if k in line:
-                result[k] += 1
+        if separate_string[-2] in result:
+            result[separate_string[-2]] += 1
         count += 1
         if count % 10 == 0:
             print("File size: {}".format(filesize))
@@ -27,6 +26,7 @@ try:
     for k, v in sorted(result.items()):
         if (v != 0):
             print("{}: {}".format(k, v))
+
 except KeyboardInterrupt:
     print("File size: {}".format(filesize))
     for k, v in sorted(result.items()):
