bb04e08e924c119d101eb7db70b3673f97d7f362
jodt <joel.dumortier@gmail.com>
2022-03-09 15:27:11 +0000

    refactoring code

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -3,7 +3,5 @@ SELECT tv_genres.name as genre, COUNT(*) as number_of_shows
 FROM tv_genres
 INNER JOIN tv_show_genres
 ON tv_show_genres.genre_id = tv_genres.id
-INNER JOIN tv_shows
-ON tv_show_genres.show_id = tv_shows.id
 GROUP BY 1
 ORDER BY 2 DESC;
