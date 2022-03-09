7d9e15c2dfccb2b039a6d2968b637fb463d0abf0
jodt <joel.dumortier@gmail.com>
2022-03-09 14:28:14 +0000

    bug fix alias

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,5 +1,5 @@
 -- lists all genres from hbtn_0d_tvshows and displays the number of shows linked to each.
-SELECT tv_genres.name as genre_id, COUNT(*) as number_of_shows
+SELECT tv_genres.name as genre, COUNT(*) as number_of_shows
 FROM tv_shows
 RIGHT JOIN tv_show_genres
 ON tv_show_genres.show_id = tv_shows.id
