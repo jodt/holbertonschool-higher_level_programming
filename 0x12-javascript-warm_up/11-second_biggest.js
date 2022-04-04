9f287ae2dd0cb2598b3b2b48c23d480e0937078a
jodt <joel.dumortier@gmail.com>
2022-04-04 13:17:53 +0000

    bug fix with negatives numbers

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,11 +1,12 @@
 #!/usr/bin/node
 const arr = [];
 if (process.argv.length === 2 || process.argv.length === 3) {
-  console.log('0');
+    console.log('0');
 } else if (process.argv.length > 3) {
-  for (let i = 2; i < process.argv.length; i++) {
-    arr.push(process.argv[i]);
-  }
-  arr.sort();
-  console.log(arr[arr.length - 2]);
+    for (let i = 2; i < process.argv.length; i++) {
+        arr.push(process.argv[i]);
+    }
+    arr.sort((a, b) => a - b);
+    console.log(arr)
+    console.log(arr[arr.length - 2]);
 }
