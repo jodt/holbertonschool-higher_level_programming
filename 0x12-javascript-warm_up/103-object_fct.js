f5e92da5d785ac91b0e6ab370c0a1682ecd878bd
jodt <joel.dumortier@gmail.com>
2022-04-04 15:09:45 +0000

    semistandard bug fix

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 const myObject = {
-    type: 'object',
-    value: 12
+  type: 'object',
+  value: 12
 };
 console.log(myObject);
-myObject.incr = function () { myObject.value++ };
+myObject.incr = function () { myObject.value++; };
 myObject.incr();
 console.log(myObject);
 myObject.incr();
