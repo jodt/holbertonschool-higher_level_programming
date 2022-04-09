0f675511f8a94754c1246c9fa7319d6aab4d9318
jodt <joel.dumortier@gmail.com>
2022-04-09 17:35:47 +0000

    documentation bug fix

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -9,6 +9,9 @@ from relationship_state import Base
 
 
 class City (Base):
+    """
+    This is the City class
+    """
     __tablename__ = 'cities'
     id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -11,6 +11,9 @@ Base = declarative_base()
 
 
 class State(Base):
+    """
+    This is the State class
+    """
     __tablename__ = "states"
     id = Column(Integer, primary_key=True)
     name = Column(String(128),  nullable=False)
