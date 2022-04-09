78967942ad787e3e643558de06b33e5e0092930c
jodt <joel.dumortier@gmail.com>
2022-04-09 17:20:50 +0000

    bug fix case sensitive

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
         db=sys.argv[3],
         charset="utf8")
     cur = db.cursor()
-    cur.execute('SELECT * FROM states WHERE name LIKE "N%"')
+    cur.execute('SELECT * FROM states WHERE name LIKE BINARY "N%"')
     rows = cur.fetchall()
     for row in rows:
         print(row)
