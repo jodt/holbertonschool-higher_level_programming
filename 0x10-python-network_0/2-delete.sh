9788d8029d4a120eee048541bafaeecf1c82a191
jodt <joel.dumortier@gmail.com>
2022-04-21 11:27:42 +0000

    delete echo command

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #script that sends a DELETE request to the URL passed and displays the body of the response
-curl -sX DELETE "$1" ; echo ""
+curl -sX DELETE "$1"
--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #script that takes in a URL, sends a POST, and displays the body of the response
-curl -sX POST -d "email=test@gmail.com" -d "subject=I will always be here for PLD" "$1"; echo ""
+curl -sX POST -d "email=test@gmail.com" -d "subject=I will always be here for PLD" "$1"
