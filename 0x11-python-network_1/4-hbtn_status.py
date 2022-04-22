56278e0f7eda48e384ae5f98f2d87c3396350d34
jodt <joel.dumortier@gmail.com>
2022-04-22 09:28:28 +0000

    add line body response

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -6,5 +6,6 @@ script that fetches https://intranet.hbtn.io/status
 import requests
 if __name__ == '__main__':
     r = requests.get('https://intranet.hbtn.io/status')
-    print(("\t- type: {}".format(type(r.encoding))))
+    print("Body response:")
+    print(("\t- type: {}".format(type(r.text))))
     print(("\t- content: {}".format(r.text)))
