fe7c65e58a72500ccfe49a58660b7225eaf419d9
jodt <joel.dumortier@gmail.com>
2022-04-22 15:20:18 +0000

    add task 10

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+This is the module github_commits
+"""
+import requests
+import sys
+if __name__ == '__main__':
+    user = sys.argv[2]
+    repo = sys.argv[1]
+    url = 'https://api.github.com/repos/{}/{}/commits'.format(repo, user)
+    r = requests.get(url)
+    list = r.json()
+    for i in range(10):
+        print("{}: {}".format(list[i].get('sha'), list[i].get(
+            'commit').get('author').get("name")))
