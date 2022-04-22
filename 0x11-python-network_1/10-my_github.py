3720beb9086f3b9644dae91babd17ae084cfe382
jodt <joel.dumortier@gmail.com>
2022-04-22 14:38:21 +0000

    bug fix

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -7,9 +7,8 @@ and uses the GitHub API to display your id
 import requests
 import sys
 if __name__ == '__main__':
-    url = 'https://api.github.com/users/octocat'
-    token = sys.argv[2]
+    url = 'https://api.github.com/user'
     username = sys.argv[1]
-    r = requests.get(url, data={username: token})
-    dict = r.json()
-    print(dict.get('id'))
+    token = sys.argv[2]
+    r = requests.get(url, auth=(username, token))
+    print(r.json().get('id') if r.ok else "None")
