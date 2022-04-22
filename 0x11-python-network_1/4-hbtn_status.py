0de5e148c1a8d1faa58713508402a8b99d02a8da
jodt <joel.dumortier@gmail.com>
2022-04-22 09:10:08 +0000

    add task 4

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+This is the hbtn_status module
+script that fetches https://intranet.hbtn.io/status
+"""
+import requests
+if __name__ == '__main__':
+    r = requests.get('https://intranet.hbtn.io/status')
+    print(("\t- type: {}".format(type(r.text))))
+    print(("\t- content: {}".format(r.text)))
