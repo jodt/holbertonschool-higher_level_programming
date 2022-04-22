a097174aa31a2f28dac6aeb6cc71777ede600096
jodt <joel.dumortier@gmail.com>
2022-04-22 12:36:00 +0000

    add task 8

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+This is the module json_api
+The script takes in a letter and sends a
+POST request to http://0.0.0.0:5000/search_user
+with the letter as a parameter.
+"""
+import sys
+import requests
+if __name__ == '__main__':
+    if len(sys.argv) == 1:
+        print("No result")
+    else:
+        url = "http://0.0.0.0:5000/search_user"
+        value = {'q': sys.argv[2] if sys.argv[2] else ''}
+        try:
+            r = requests.post(url, data=value).json()
+            if len(r):
+                print("[{}] {}".format(r['id'], r['name']))
+            else:
+                print("No result")
+        except requests.exceptions.JSONDecodeError as e:
+            print(e)
