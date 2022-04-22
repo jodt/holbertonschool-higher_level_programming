def149820963d0ffc6879a49a8a0851c849c4ab6
jodt <joel.dumortier@gmail.com>
2022-04-22 16:05:48 +0000

    refactoring code with param per_page

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -8,11 +8,11 @@ if __name__ == '__main__':
     user = sys.argv[2]
     repo = sys.argv[1]
     url = 'https://api.github.com/repos/{}/{}/commits'.format(repo, user)
-    r = requests.get(url)
+    r = requests.get(url, params={'per_page': 10})
     if r.ok:
         list = r.json()
-        for i in range(10):
-            print("{}: {}".format(list[i].get('sha'), list[i].get(
+        for commit in list:
+            print("{}: {}".format(commit.get('sha'), commit.get(
                 'commit').get('author').get("name")))
     else:
         print("None")
