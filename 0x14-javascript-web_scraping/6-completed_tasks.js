045e05e82adb0dc464212615feb273e42d5e198f
jodt <joel.dumortier@gmail.com>
2022-05-17 13:01:08 +0000

    refactoring code bug fix

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -5,17 +5,12 @@ const axios = require('axios').default;
 axios.get(process.argv[2])
   .then(response => {
     const result = {};
-    let count;
-    let countTemp;
     response.data.forEach(task => {
-      if (task.userId in result) {
-        count = countTemp;
-      } else {
-        count = 0;
-      }
       if (task.completed) {
-        result[task.userId] = ++count;
-        countTemp = count;
+        if (task.userId in result)
+          result[task.userId]++;
+        else
+        result[task.userId] = 1;
       }
     });
     console.log(result);
