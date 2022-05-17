f125073f8a26547b42e96c164f2a18061784a6d0
jodt <joel.dumortier@gmail.com>
2022-05-17 12:00:40 +0000

    bug fix console.log

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -2,5 +2,5 @@
 const axios = require('axios').default;
 
 axios.get(process.argv[2])
-  .then(response => console.log(response.status))
-  .catch(err => console.log(err.response.status));
+  .then(response => console.log("code: " + response.status))
+  .catch(err => console.log("code: " + err.response.status));
