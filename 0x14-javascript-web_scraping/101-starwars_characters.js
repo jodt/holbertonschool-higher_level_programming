336d6e73bc47544bc2ed46a002d4d5dda1ff6c99
jodt <joel.dumortier@gmail.com>
2022-05-17 14:34:38 +0000

    bug fix async function

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -3,19 +3,12 @@
 const axios = require('axios').default;
 
 axios.get('https://swapi-api.hbtn.io/api/films')
-  .then(response => {
+  .then(async response => {
     const index = (process.argv[2] - 1);
     const film = response.data.results[index];
     for (const character of film.characters) {
-      async function response (character) {
-        try {
-          const resp = await axios.get(character);
-          console.log(resp.data.name);
-        } catch (err) {
-          console.log(err);
-        }
-      }
-      response(character);
+      const resp = await axios.get(character);
+      console.log(resp.data.name);
     }
   }
   );
